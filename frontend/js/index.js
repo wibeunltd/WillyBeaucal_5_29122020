5449b4cab532eba5e31af30df1dda8131ff0b225
wibeunltd <wibe@hotmail.fr>
2021-01-24 18:29:32 +0100

    index page completed, product page started

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,19 +1,32 @@
-/* 
-Récupération avec la méthode Fetch
-Création d'une constante nommée qui contiendra l'url api
-Fonction asynchrone car on doit attendre la récupération des données via l'api
-*/
-const apiUrl = "http://localhost:3000/api/teddies";
+const apiUrl = "http://localhost:3000/api/teddies"; //Constante nommée apiURL, contient l'url de l'api
 
-const getTeddies = async function () {
+const getProduct = async function () { //Fonction asynchrone getProduct via la méthode fetch, récupère les produits
   try {
     let response = await fetch(apiUrl);
-    if (response.ok) {
+    if (response.ok) { // Réponse positive, récupération des produits
       let products = await response.json();
       console.log(products);
+      let teddiesCards = ""; // Création des cards html
       products.forEach((product) => {
         console.log(product);
+        teddiesCards +=
+          `<div class="col-12 col-lg-4">
+        <div class="card mb-4 mb-lg-4 border-light shadow">
+          <img src="${product.imageUrl}" alt="Ours en peluche ${product.name}"/>
+          <div class="card-body">
+            <h2 class="card-title">${product.name}</h2>
+            <p class="card-text">${product.price} €</p>
+            <a href="produit.html?id=${product._id}" class="btn btn-info stretched-link"
+              >Découvrir ${product.name}</a
+            >
+          </div>
+        </div>
+      </div>`;
       });
+      console.log(teddiesCards);
+      let elt = document.getElementById('teddies');
+      elt.innerHTML = teddiesCards;
+
     } else {
       console.error(
         "Une erreur " + response.status + " a été retournée par le serveur."
@@ -22,8 +35,6 @@ const getTeddies = async function () {
   } catch (e) {
     console.log(e);
   }
-
-  //Test ajout d'informations
 };
 
-getTeddies();
+getProduct();
